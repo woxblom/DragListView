51815c39bca2ee014f555c57f28bed1729ffd923
ceekays <kachaleedmond@gmail.com>
2017-11-27 16:29:33 +0200

    Added a method to expose RecyclerView's addItemDecoration method

--- library/src/main/java/com/woxthebox/draglistview/DragListView.java
+++ library/src/main/java/com/woxthebox/draglistview/DragListView.java
@@ -289,6 +289,10 @@ public class DragListView extends FrameLayout {
         mRecyclerView.setScrollingEnabled(scrollingEnabled);
     }
 
+    public void addItemDecoration(RecyclerView.ItemDecoration decor){
+        mRecyclerView.addItemDecoration(decor);
+    }
+
     /**
      * Set if items should not reorder automatically when dragging. If reorder is disabled, drop target
      * drawables can be set with {@link #setDropTargetDrawables} which will highlight the current item that
